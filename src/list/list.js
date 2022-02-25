6a3a3b0b5318747f02b6bb15639e10d8963d5df9
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2022-02-26 00:08:01 +0500

    asdf

--- src/list/list.js
+++ src/list/list.js
@@ -59,13 +59,36 @@ import React, { Component } from 'react'
 // for state using map.....
 
 export default class List extends Component {
+    
+    state = {
+        user:[
+            {id: 101, name:"mallik", password:"1352"},
+            {id: 102, name:"ashar", password:"1112"},
+            {id: 103, name:"owais", password:"1222"}
+    ]
+    }
+    
     render() {
-        const arr = this.props.numbers;
-  
+           const newUser = this.state.user.map(usr => {
+               console.log(usr);
+               return (
+                   <>
+                      <h1>ID: {usr.id}</h1>
+                      <h1>Name: {usr.name}</h1>
+                      <h1>Password: {usr.password}</h1> 
+                   </>  
+               )
+           })
       return (
-        <ul>
-          { arr.map(num => (<li>{num*2}</li>)) }
-        </ul>
+        <div>
+          {/* <h1>ID: {this.state.user[0].id}</h1>
+          <h1>Name: {this.state.user[0].name}</h1>
+          <h1>Password: {this.state.user[0].password}</h1>  */}
+
+          {/* or using map method */}
+
+          {newUser}
+        </div>
       )
     }
   }
