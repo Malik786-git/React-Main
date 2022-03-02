b861acaeb1a06274054af2edb37aaa627f507aba
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2022-03-02 22:55:12 +0500

    updateed

--- src/useAssets/Assets.js
+++ src/useAssets/Assets.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react'
 import pic from '../python.jpg'
 // Image in public foldre
 export default class Assets extends Component {
+
   render()
    {
     return (
-      <div>
+      <React.Fragment>
         <img 
         src={process.env.PUBLIC_URL + '/psl.jpg'} 
         alt = 'pslPicture'
         width='200px' />
-      </div>
+      </React.Fragment>
     )
   }
+
 }
+
+
 
 // Image in src folder
 // export default class Assets extends Component {
